c02f878a16483e03cf8bda67d55dd562d9be006a
A-Tomy-k <avaqueroblanco@gmail.com>
2014-06-13 01:19:48 +0200

    Index
    First version of index.php
    
    Signed-off-by: A-Tomy-k <avaqueroblanco@gmail.com>

--- web/index.php
+++ web/index.php
@@ -1,5 +1,18 @@
 <?php
+require_once __DIR__.'/../vendor/autoload.php';
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
+$app = new Silex\Application();
+
+$app->mount('/mobile', new Controllers\AndroidController());
+
+$app->get('/', function() use($app) {
+	return new Response('0K',200);
+})->bind('portada');
+
+$app->run();
+/*
 ini_set('display_errors', 0);
 
 require_once __DIR__.'/../vendor/autoload.php';
@@ -8,3 +21,4 @@ $app = require __DIR__.'/../src/app.php';
 require __DIR__.'/../config/prod.php';
 require __DIR__.'/../src/controllers.php';
 $app->run();
+*/
